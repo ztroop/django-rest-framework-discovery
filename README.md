7243ee4c42673faa4fe6781e86ac499624df77cc
ztroop <zackary.troop@esentire.com>
2018-04-20 11:31:47 -0400

    Add travis/tox integration.

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 ## Django Rest Framework Discovery
 
+[![Build Status](https://travis-ci.org/ztroop/django-rest-framework-discovery.svg?branch=master)](https://travis-ci.org/ztroop/django-rest-framework-discovery)
+
 Discovery allows you to create an API from an existing database with little to no effort. This project is based on Shabda Raaj's [Bookrest][1]. You can then leverage the capabilties of the Django Rest Framework to apply [filtering][2], [pagination][3] and generate [documentation][4].
 
 [1]: https://github.com/agiliq/bookrest
--- setup.py
+++ setup.py
@@ -4,11 +4,23 @@ version = "0.1.5"
 
 setup(
     author='Zackary Troop',
-    name='rest_framework_discovery',
+    name='djangorestframework-discovery',
     version=version,
     url='https://github.com/ztroop/djangorestframework-discovery',
     license='BSD',
-    description='Automatically creates a queryable data source from an existing database.',
+    description='Create a queryable data source from an existing database.',
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
     packages=find_packages(exclude=['assets']),
     install_requires=['django>=2.0.0', 'djangorestframework>=3.8.0'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+    ]
 )
